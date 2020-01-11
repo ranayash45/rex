6836dc4e206c34cdf9ca4ab25fe8d18f646c9baf
Yash Rana <y.rana18@outlook.com>
2020-01-11 21:36:56 +0530

    new commit from scratch

--- rex/settings.py
+++ rex/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'Resource'),
 )
 
-STATIC_ROOT = "/var/www/static"
-MEDIA_ROOT = "/var/www/media"
-#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-#MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+#STATIC_ROOT = "/var/www/static"
+#MEDIA_ROOT = "/var/www/media"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
