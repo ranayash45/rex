14f6d0d68180cb36a1f9cb9e87194c9d154de456
Yash Rana <y.rana18@outlook.com>
2020-01-11 20:42:06 +0530

    new commit from scratch

--- rex/settings.py
+++ rex/settings.py
@@ -123,10 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
@@ -134,3 +130,6 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'Resource'),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
